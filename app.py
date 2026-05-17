9d42ae715c5fd2bbacb73a1467e395fd371b62df
Mrinal Dutta <mrinal.dutta1@ibm.com>
2026-05-17 13:31:39 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+# add function here
+def add(a, b):  
+    return a + b
+
+# add main function to run
+if __name__ == "__main__":  
+    print(add(1, 2))  # should print 3
+    print(add(-1, 1)) # should print 0
+    print(add(0, 0))  # should print 0
